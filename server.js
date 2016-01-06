e398fef9e333d21b738a9325ee807f6e25df5bc3
gazzer82 <contact@garethjeanne.co.uk>
2016-01-05 16:18:55 -0800

    Update server.js
    Added devtool: 'eval' and set output.pathinfo:true

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  devtool: "eval",
+  output.pathinfo: true
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
